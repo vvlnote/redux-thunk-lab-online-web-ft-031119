69107f6d119c9b64a3eeb3af338fd05e1fd9c309
Joy Lin <vvlnote@hotmail.com>
2019-09-05 22:30:25 +0000

    Done.

--- src/actions/catActions.js
+++ src/actions/catActions.js
@@ -4,5 +4,5 @@ export function fetchCats(){
 		return fetch('http://localhost:4000/db')
 				.then(response => response.json())
 				.then(cats => dispatch({type: 'FETCH_CATS', payload: cats.images}));
-	}
+	};
 }
